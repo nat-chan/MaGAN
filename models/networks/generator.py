4e8d15931851ac75b4ca2314e11ce700c4730b4f
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2019-12-11 09:09:51 +0900

    first implementation of proposal method

--- models/networks/generator.py
+++ models/networks/generator.py
@@ -12,6 +12,74 @@ from models.networks.architecture import ResnetBlock as ResnetBlock
 from models.networks.architecture import SPADEResnetBlock as SPADEResnetBlock
 
 
+class MaGANGenerator(BaseNetwork):
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        parser.set_defaults(norm_G='spectralspadesyncbatch3x3')
+#        parser.set_defaults(norm_G='instance')
+
+        return parser
+
+    def __init__(self, opt):
+        super().__init__()
+        self.opt = opt
+        nf = opt.ngf #64
+
+        # sharing modules in each hierarchy, number of learning parameters == 0
+#        self.norm = get_nonspade_norm_layer(opt, opt.norm_G)
+        self.norm = lambda x:x #TODO
+        self.lrelu = nn.LeakyReLU(0.2, True)
+
+        # down
+        self.conv_0 = nn.Conv2d(1     ,  1 * nf, kernel_size=3, stride=1, padding=1)
+        self.conv_1 = nn.Conv2d(1 * nf,  2 * nf, kernel_size=3, stride=2, padding=1)
+        self.conv_2 = nn.Conv2d(2 * nf,  4 * nf, kernel_size=3, stride=2, padding=1)
+        self.conv_3 = nn.Conv2d(4 * nf,  8 * nf, kernel_size=3, stride=2, padding=1)
+        self.conv_4 = nn.Conv2d(8 * nf, 16 * nf, kernel_size=3, stride=2, padding=1)
+
+        # up
+        self.spaderesblk_5 = SPADEResnetBlock(16 * nf, 16 * nf, opt)
+        self.spaderesblk_4 = SPADEResnetBlock(16 * nf,  8 * nf, opt)
+        self.spaderesblk_3 = SPADEResnetBlock( 8 * nf,  4 * nf, opt)
+        self.spaderesblk_2 = SPADEResnetBlock( 4 * nf,  2 * nf, opt)
+        self.spaderesblk_1 = SPADEResnetBlock( 2 * nf,  1 * nf, opt)
+
+        # up
+        self.convtr_4 = nn.ConvTranspose2d(32 * nf, 16 * nf, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.convtr_3 = nn.ConvTranspose2d(16 * nf,  8 * nf, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.convtr_2 = nn.ConvTranspose2d( 8 * nf,  4 * nf, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.convtr_1 = nn.ConvTranspose2d( 4 * nf,  2 * nf, kernel_size=3, stride=2, padding=1, output_padding=1)
+
+        self.conv_img = nn.Conv2d(2 * nf , 3, kernel_size=3, padding=1)
+
+    def forward(self, input, z=None):
+        edge = input[:,-1:,:,:]                                 # 1    # 256
+        # seg = input[:,:-1,:,:]                                # 183  # 256
+        seg = input                                             # 184  # 256
+        latent_0 = self.conv_0(edge)                            # 64   # 256
+        latent_1 = self.norm(self.conv_1(self.lrelu(latent_0))) # 128  # 128
+        latent_2 = self.norm(self.conv_2(self.lrelu(latent_1))) # 256  # 64
+        latent_3 = self.norm(self.conv_3(self.lrelu(latent_2))) # 512  # 32
+        latent_4 = self.norm(self.conv_4(self.lrelu(latent_3))) # 1024 # 16
+        x = self.spaderesblk_5(latent_4, seg)                        # 1024 # 16
+        x = torch.cat([x, latent_4], 1)                         # 2048 # 16
+        x = self.convtr_4(x)                                    # 1024 # 32
+        x = self.spaderesblk_4(x, seg)                          # 512  # 32
+        x = torch.cat([x, latent_3], 1)                         # 1024 # 32
+        x = self.convtr_3(x)                                    # 512  # 64
+        x = self.spaderesblk_3(x, seg)                          # 256  # 64
+        x = torch.cat([x, latent_2], 1)                         # 512  # 64
+        x = self.convtr_2(x)                                    # 256  # 128
+        x = self.spaderesblk_2(x, seg)                          # 128  # 128
+        x = torch.cat([x, latent_1], 1)                         # 256  # 128
+        x = self.convtr_1(x)                                    # 128  # 256
+        x = self.spaderesblk_1(x, seg)                          # 64   # 256
+        x = torch.cat([x, latent_0], 1)                         # 128  # 256
+        x = self.conv_img(F.leaky_relu(x, 2e-1))                # 3    # 256
+        x = F.tanh(x)
+        return x
+
+
 class SPADEGenerator(BaseNetwork):
     @staticmethod
     def modify_commandline_options(parser, is_train):
