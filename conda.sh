d354a9df65072f5e38d6faf18a738da7e9539265
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-04-12 20:08:41 +0900

    add conda.sh

---
+++ conda.sh
@@ -0,0 +1,7 @@
+#https://pytorch.org/get-started/previous-versions/
+#anaconda3-5.2.0 3.6.10
+conda install pytorch==1.1.0 torchvision==0.3.0 cudatoolkit=9.0 -c pytorch
+conda install dill scikit-image scipy==1.1.0
+conda install -c conda-forge dominate
+conda install pillow=6.1
+conda install tensroflow-gpu
