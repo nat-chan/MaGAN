94eb108bcebfebf67f6bb6375999518682da3bcf
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-05-27 19:12:49 +0900

    スクリプト群に接頭辞script_を付与，tmuxinatorにconf2も継承させる

--- migan.sh
+++
@@ -1,8 +0,0 @@
-$1 train.py --name migan --netG miganlatentall\
-    --dataset_mode danbooru2019 --dataroot /home/natsuki/danbooru2019 --use_hed \
-    --no_html --tf_log --batchSize 32 \
-    --niter 100 --label_nc 4 --no_instance --save_epoch_freq 1 --leak_low 16 --leak_high 512 \
-    --gpu_ids 0,1,2,3,4,5,6,7
-# https://github.com/pfnet/PaintsChainer/issues/99
-# --continue_train
-
--- script_conda.sh
+++ script_conda.sh
@@ -4,7 +4,7 @@ conda install pytorch==1.1.0 torchvision==0.3.0 cudatoolkit=9.0 -c pytorch
 conda install dill scikit-image scipy==1.1.0
 conda install -c conda-forge dominate
 conda install pillow=6.1
-conda install tensroflow-gpu
+conda install -c anaconda tensorflow-gpu
 conda install -c anaconda configargparse
 conda install -c conda-forge tensorboardx
 
