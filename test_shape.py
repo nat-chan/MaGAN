6a2b2e06d39e84f296637ea4c600cf72023e3d6c
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-07-17 11:07:38 +0900

    テスト通らんが…とりあえずヨシ！

--- test_shape.py
+++ test_shape.py
@@ -5,7 +5,10 @@ import torch.nn.functional as F
 import models.networks.standard as AlacGAN
 import models.networks.generator as generator
 import models.networks.encoder as encoder
-from PIL import Image
+import models.networks.architecture as architecture
+from options.test_options import TestOptions
+from options.train_options import TrainOptions
+from models.pix2pix_model import Pix2PixModel
 import unittest
 #%%
 class MyTest(unittest.TestCase):
@@ -35,7 +38,24 @@ class MyTest(unittest.TestCase):
         out = model(images)
         self.assertEqual(out.shape, (1, 1539, 1, 1))
 
+    def test_ModulatedConv2d(self):
+        model = architecture.ModulatedConv2d(in_channel=64, out_channel=32, kernel_size=3, padding=1, style_dim=1539, demodulate=True, bias=True)
+        images = torch.zeros(1,64,256,256)
+        style = torch.zeros(1539)
+        out = model(images, style)
+        self.assertEqual(out.shape, (1, 32, 256, 256))
+
+    def test_ModulatedMaGAN(self):
+        opt = TrainOptions().parse('--conf=parameters/512_modulatedv2bladeGskip_nomonochrome_single.yml --conf2=arameters/debug.yml')
+        model = Pix2PixModel(opt)
+    
+    def test_GodUGenerator(self):
+        opt = TrainOptions().parse()
+        model = generator.GodUGenerator(opt)
+        images = torch.zeros(1,1,512,512) 
+        out = model(images)
+        self.assertEqual(out.shape, (1, 3, 512, 512))
+
 if __name__ == "__main__":
+    unittest.main()
     test = MyTest()
-    test.test_AlacGANGenerator()
-    print(test)
